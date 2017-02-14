ed9e80282bc8c0a1cfd9f5320fd11766bdfa743a
maxmarinich <max.marinich@gmail.com>
2017-02-14 15:03:03 +0300

    add folders structure

---
+++ assets/js/log.js
@@ -0,0 +1,13 @@
+import winston from 'winston';
+
+const log = winston.loggers.add('log', {
+  console: {
+    level: 'info',
+    colorize: true,
+    timestamp: true,
+    prettyPrint: true,
+    humanReadableUnhandledException: true
+  }
+});
+
+export default log;
