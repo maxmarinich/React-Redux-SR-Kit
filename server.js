d194020a805537fe71532d66e56925f7efc10056
maxmarinich <max.marinich@gmail.com>
2017-02-15 12:00:40 +0300

    add template engine

--- server.js
+++ server.js
@@ -10,8 +10,11 @@ const port = process.env.PORT || 8080;
 app.use(cookieParser());
 app.use(Express.static(path.join(__dirname, 'static')));
 
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'pug');
+
 app.get('*', (req, res) => {
-  res.send(renderPage());
+  res.render('index', { html: 'root', state: 'state' })
 });
 
 app.listen( port, (error) => {
@@ -21,27 +24,3 @@ app.listen( port, (error) => {
     log.info(`==> 🌎  Listening on port ${port}. Open up http://localhost:${port}/ in your browser.`)
   }
 });
-
-function renderPage(html, state) {
-  return `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>React Kit</title>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width initial-scale=1">
-        <link rel="shortcut icon" href="/i/favicon.ico" type="image/x-icon">
-        <link type="text/css"  rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-        <link rel="stylesheet" type="text/css" href="/style.css">
-      </head>
-      <body>
-        <div id="root">${html || 'root'}</div>
-        <script>
-          window.__PRELOADED_STATE__ = ${JSON.stringify(state || 'state')}
-        </script>
-        <script src="/index.js"></script>
-      </body>
-    </html>
-    `
-}
